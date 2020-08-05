b746a66d70fccbe069080da0c06364383e7c6333
Joshua Jacobs <joshuajacobs@flywheel.io>
2020-08-05 14:28:18 -0500

    Remove test directory reference

--- run.py
+++ run.py
@@ -97,8 +97,7 @@ if __name__ == "__main__":
     )
     logging.basicConfig(level=log_level, format=fmt, datefmt="%H:%M:%S")
     log.info("Log level is {}".format(log_level))
-    tst = "/Users/joshuajacobs/Projects/2020.01.29.ROI2nii/Data/roi2nix-0.2.0_5f29d1524cf00200f4633d0a"
-    with flywheel.GearContext(tst) as gear_context:
+    with flywheel.GearContext() as gear_context:
         gear_context.log = log
         gear_context.log_config()
         exit_status = main(gear_context)
